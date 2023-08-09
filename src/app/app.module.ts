7132681c06192ea4dd8d0babc14d259ea89397e6
Taras <sokha2411@gmail.com>
2023-08-09 12:18:41 +0300

    feat(15): create components,add some global styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { MainComponent } from './components/main/main.component';
+import { QuizListComponent } from './components/quiz-list/quiz-list.component';
+import { QuizCardComponent } from './components/quiz-card/quiz-card.component';
+import { IntroductionComponent } from './components/introduction/introduction.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    MainComponent,
+    QuizListComponent,
+    QuizCardComponent,
+    IntroductionComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
