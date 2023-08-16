9235d7d23560b96593c708b963645566aafec02b
Taras <sokha2411@gmail.com>
2023-08-16 12:14:36 +0300

    feat(16) commas

--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
@@ -8,7 +7,7 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [HomeComponent],
+      declarations: [HomeComponent]
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
