3ff4a6e6ec08427c3f8b98a0567e4bc84f967621
Taras <sokha2411@gmail.com>
2023-08-16 17:45:59 +0300

    feat(28) add variables for styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,13 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     colors: {
-      primary: "#e6e6ff",
-      buttons: "#E5E7EB",
-      "buttons-hover": "#D1D5DB",
-      "error-message": "#D10000"
+      primary: "var(--color-primary)",
+      buttons: "var(--color-button)",
+      "buttons-hover": "var(--color-buttons-hover)",
+      "error-message": "var(--color-error-message)"
     },
     "background-image": {
-      "gradient-to-bottom": "linear-gradient(to bottom, #c7c7ff, #b3b3ff)"
+      "gradient-to-bottom": "var(--background-image)"
     },
     extend: {}
   },
