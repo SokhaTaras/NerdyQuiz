2ac9b8887367f3bfd6692da797749a8b5906e240
Taras <sokha2411@gmail.com>
2023-08-16 14:19:18 +0300

    feat(16) move common styles to variables, make tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    colors: {
+      primaryBackground: "#e6e6ff",
+      popUpBackground: "",
+      buttons: "#E5E7EB",
+      buttonsHover: "#D1D5DB",
+      errorMessage: "#D10000"
+    },
+    cursor: {
+      "not-allowed": "not-allowed"
+    },
+    backgroundImage: {
+      "gradient-to-bottom": "linear-gradient(to bottom, #c7c7ff, #b3b3ff)"
+    },
+    extend: {}
   },
-  plugins: [],
-}
+  plugins: []
+};
