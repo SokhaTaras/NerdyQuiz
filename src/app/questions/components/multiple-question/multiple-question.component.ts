de040236abca9f85239d118d0d2fc08d78e11c89
Taras <sokha2411@gmail.com>
2023-09-11 14:58:35 +0300

    feat(35) merge helper service

--- src/app/questions/components/multiple-question/multiple-question.component.ts
+++ src/app/questions/components/multiple-question/multiple-question.component.ts
@@ -3,15 +3,12 @@ import { FormGroup } from '@angular/forms';
 
 import { PlaceHolder } from '../../../shared/enums/placeHolder';
 import { QuestionForm } from '../../../shared/interfaces/forms';
-import { DifficultyList, TypeList } from '../../constants/dropdonws';
-import { AnswersFormType } from '../../../shared/types/formsType';
 import {
   maxQuestionsAmount,
   minQuestionsAmount
 } from '../../constants/questions-info';
 import { BUTTON_TYPE } from '../../../shared/enums/buttonType';
 import { AnswerDifficultyList } from '../../constants/dropdonws';
-import { maxQuestions } from '../../constants/max-questions';
 import { QuestionFormHelperService } from '../../../shared/services/questionFormHelper/question-form-helper.service';
 import {
   ANSWER_PROPERTIES,
@@ -29,12 +26,11 @@ export class MultipleQuestionComponent implements OnInit {
     new EventEmitter<FormGroup<QuestionForm>>();
 
   readonly PlaceHolder = PlaceHolder;
-  readonly maxQuestionsAmount = maxQuestions;
   readonly AnswerDifficultyList = AnswerDifficultyList;
   readonly ANSWER_PROPERTIES = ANSWER_PROPERTIES;
-  protected readonly maxQuestionsAmount = maxQuestionsAmount;
-  protected readonly minQuestionsAmount = minQuestionsAmount;
-  protected readonly BUTTON_TYPE = BUTTON_TYPE;
+  readonly maxQuestionsAmount = maxQuestionsAmount;
+  readonly minQuestionsAmount = minQuestionsAmount;
+  readonly BUTTON_TYPE = BUTTON_TYPE;
 
   get form(): FormGroup<QuestionForm> {
     return this.questionFormHelper?.currentForm;
@@ -47,13 +43,13 @@ export class MultipleQuestionComponent implements OnInit {
   }
 
   addAnswer(): void {
-    if (maxQuestions > this.questionFormHelper.answersCount) {
+    if (maxQuestionsAmount > this.questionFormHelper.answersCount) {
       this.questionFormHelper.addAnswer();
     }
   }
 
   deleteAnswer(answerIndex: number): void {
-    this.answersControl.splice(answerIndex, 1);
+    this.questionFormHelper.answersFormArray.controls.splice(answerIndex, 1);
   }
 
   private initForm(): void {
