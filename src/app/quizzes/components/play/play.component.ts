9291609b9d7df93248f31c433c11e79ecb273cfd
Taras <sokha2411@gmail.com>
2023-09-15 11:14:50 +0300

    feat/(7) use 2 buttons instead of 4, changed timer to rxJs one, add subscription service.

--- src/app/quizzes/components/play/play.component.ts
+++ src/app/quizzes/components/play/play.component.ts
@@ -1,38 +1,44 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Observable, timer } from 'rxjs';
 
 import { Answer, Question } from '../../../questions/interfaces/question';
 import { BUTTON_TYPE } from '../../../shared/enums/buttonType';
 import { ModalQuizService } from '../../services/modal-quiz/modal-quiz.service';
 import { QuizService } from '../../services/quiz/quiz.service';
 import { NavigateToService } from '../../../shared/services/navigate-to/navigate-to.service';
+import { SubscriptionsService } from '../../../shared/services/subscription/subscriptions.service';
 
 @Component({
   selector: 'quiz-app-play',
   templateUrl: './play.component.html',
-  styleUrls: ['./play.component.scss']
+  styleUrls: ['./play.component.scss'],
+  providers: [SubscriptionsService]
 })
 export class PlayComponent implements OnInit {
   @Input() questions: Question[];
   @Output() whenCancel: EventEmitter<void> = new EventEmitter();
 
+  timer$: Observable<number>;
+
   currentQuestion: Question;
   selectedAnswer: Answer | null = null;
 
   currentPosition = 0;
-  timePerAnswer: number;
+  secondsCounter = 0;
 
   readonly BUTTON_TYPE = BUTTON_TYPE;
 
   constructor(
     private modalQuizService: ModalQuizService,
     private quizService: QuizService,
-    private navigateTo: NavigateToService
+    private navigateTo: NavigateToService,
+    private subscriptions: SubscriptionsService
   ) {}
 
   ngOnInit(): void {
     this.currentQuestion = this.questions[this.currentPosition];
     this.quizService.questionsResults$.next([]);
-    this.timePerAnswer = Date.now();
+    this.startTimer();
   }
 
   cancelQuizConfirm(): void {
@@ -41,47 +47,62 @@ export class PlayComponent implements OnInit {
       buttonText: 'BUTTON.CONFIRM'
     };
 
-    this.modalQuizService
-      .confirmDeletionModal(data)
-      .onClose.subscribe((isConfirm) => {
-        if (isConfirm) {
-          this.whenCancel.emit();
-        }
-      });
+    this.subscriptions.addSubscription(
+      this.modalQuizService
+        .confirmDeletionModal(data)
+        .onClose.subscribe((isConfirm) => {
+          if (isConfirm) {
+            this.whenCancel.emit();
+          }
+        })
+    );
   }
 
-  nextQuestion(): void {
+  nextQuestion(question: Question): void {
+    this.addQuestionResult(question);
     this.currentPosition += 1;
   }
 
   previousQuestion(): void {
+    this.subscriptions.addSubscription(
+      this.quizService
+        .removeLastQuestionResult(this.currentPosition)
+        .subscribe()
+    );
     this.currentPosition -= 1;
-    this.quizService.removeLastQuestionResult(this.currentPosition).subscribe();
   }
 
   selectAnswer(answer: Answer): void {
     this.selectedAnswer = answer;
   }
 
-  //todo redirect to result page on finish, when that page will be ready
   addQuestionResult(question: Question): void {
     const maxPosition = this.questions?.length;
-    const timePerAnswer = Date.now() - this.timePerAnswer;
-    this.quizService
-      .addQuestionResult(question, this.selectedAnswer, timePerAnswer)
-      .subscribe();
+    this.subscriptions.addSubscription(
+      this.quizService
+        .addQuestionResult(question, this.selectedAnswer, this.secondsCounter)
+        .subscribe()
+    );
 
     if (this.currentPosition < maxPosition) {
       this.currentQuestion = this.questions[this.currentPosition];
       this.selectedAnswer = null;
-      this.nextQuestion();
     }
   }
 
-  //todo change, when result page will be ready
+  //todo redirect to result page on finish, when that page will be ready
   finishQuiz(lastQuestion: Question): void {
     this.addQuestionResult(lastQuestion);
-    this.quizService.setQuizResult().subscribe();
+    this.subscriptions.addSubscription(
+      this.quizService.setQuizResult().subscribe()
+    );
     this.navigateTo.navigateHome();
+  }
+
+  private startTimer(): void {
+    this.timer$ = timer(0, 1000);
+    this.subscriptions.addSubscription(
+      this.timer$.subscribe((time) => (this.secondsCounter = time))
+    );
   }
 }
