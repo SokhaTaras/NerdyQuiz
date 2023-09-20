6087281b6256fe57e606b2b8546452fd241dc200
Taras <sokha2411@gmail.com>
2023-09-20 19:21:13 +0300

    feat(47) add part of logic for deleting quiz

--- src/app/quizzes/components/quiz-card/quiz-card.component.ts
+++ src/app/quizzes/components/quiz-card/quiz-card.component.ts
@@ -3,6 +3,9 @@ import { Component, Input } from '@angular/core';
 import { Quiz } from '@a-quizzes/interfaces/quiz';
 import { BUTTON_TYPE } from '@a-shared/enums/buttonType';
 import { NavigateToService } from '@a-shared/services/navigate-to/navigate-to.service';
+import { ModalQuizService } from '@a-quizzes/services/modal-quiz/modal-quiz.service';
+import { SubscriptionsService } from '@a-shared/services/subscription/subscriptions.service';
+import { QuizService } from '@a-quizzes/services/quiz/quiz.service';
 
 @Component({
   selector: 'quiz-app-quiz-card',
@@ -14,7 +17,12 @@ export class QuizCardComponent {
 
   readonly BUTTON_TYPE = BUTTON_TYPE;
 
-  constructor(private navigateTo: NavigateToService) {}
+  constructor(
+    private navigateTo: NavigateToService,
+    private modalQuizService: ModalQuizService,
+    private subscriptionsService: SubscriptionsService,
+    private quizService: QuizService
+  ) {}
 
   goPlay(): void {
     this.navigateTo.navigatePlay(this.quiz);
@@ -23,4 +31,23 @@ export class QuizCardComponent {
   goEdit(): void {
     this.navigateTo.navigateToQuizDetailsPage(this.quiz.id);
   }
+
+  confirmRemoving(): void {
+    const data: any = {
+      text: 'CONFIRM_MODAL_TEXT.DELETE_QUIZ',
+      buttonText: 'BUTTON.CONFIRM'
+    };
+
+    this.subscriptionsService.addSubscription(
+      this.modalQuizService
+        .confirmDeletionModal(data)
+        .onClose.subscribe((isConfirm) => {
+          if (isConfirm) {
+            this.deleteQuiz();
+          }
+        })
+    );
+  }
+
+  private deleteQuiz(): void {}
 }
