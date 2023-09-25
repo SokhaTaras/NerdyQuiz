56aea970ea7db66c0fafd8b8e1bd9ab3528a2f2c
Taras <sokha2411@gmail.com>
2023-09-25 16:50:51 +0300

    feat(8) delete scss file for progress-bar

--- src/app/shared/components/progress-bar/progress-bar.component.ts
+++ src/app/shared/components/progress-bar/progress-bar.component.ts
@@ -2,8 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'quiz-app-progress-bar',
-  templateUrl: './progress-bar.component.html',
-  styleUrls: ['./progress-bar.component.scss']
+  templateUrl: './progress-bar.component.html'
 })
 export class ProgressBarComponent {
   @Input() givenWidth = 0;
