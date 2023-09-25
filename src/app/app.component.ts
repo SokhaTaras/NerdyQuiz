75475f38d3e27f1988928cd9362a35719816f8f1
Taras <sokha2411@gmail.com>
2023-09-25 14:43:33 +0300

    feat(46,47) quiz init returned

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { QuizService } from '@a-quizzes/services/quiz/quiz.service';
+import { StorageKey } from '@a-shared/enums/storageKey';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'nerdyQuiz-app';
+
+  constructor(private quizService: QuizService) {}
+
+  ngOnInit(): void {
+    this.initQuizzes();
+  }
+
+  private initQuizzes(): void {
+    this.quizService.initAllQuizzes(StorageKey.QUIZZES);
+  }
 }
