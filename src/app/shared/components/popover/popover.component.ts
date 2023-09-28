571ec039c64067e6bd470fbe1ecc38a4c0e6c3b9
Taras <sokha2411@gmail.com>
2023-09-28 11:37:50 +0300

    feat(47) alternative trigger for popover

--- src/app/shared/components/popover/popover.component.ts
+++ src/app/shared/components/popover/popover.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 
 import { POPOVER_TYPE } from '@a-shared/enums/shared-components';
 import { PopoverItem } from '@a-shared/types/popover';
+import { trigger } from '@angular/animations';
 
 @Component({
   selector: 'quiz-app-popover',
@@ -11,4 +12,5 @@ export class PopoverComponent {
   @Input() label: string;
   @Input() options: PopoverItem[];
   @Input() type: POPOVER_TYPE;
+  protected readonly trigger = trigger;
 }
