bde400884293d732cdd320a9117c36beb2793501
Taras <sokha2411@gmail.com>
2023-09-29 10:56:47 +0300

    feat(23) use service

--- src/app/store/services/store.service.spec.ts
+++ src/app/store/services/store.service.spec.ts
@@ -1,9 +1,10 @@
 import { TestBed } from '@angular/core/testing';
 
 import { StoreService } from './store.service';
+import { AppState } from '@a-store/state/app.state';
 
 describe('StoreService', () => {
-  let service: StoreService;
+  let service: StoreService<AppState>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
