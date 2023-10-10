e5104b40866c512abc6e1d41d48be44921b21030
Taras <sokha2411@gmail.com>
2023-10-10 16:23:41 +0300

    feat(52): renaming

--- src/app/shared/components/correctness-status/correctness-status.component.ts
+++ src/app/shared/components/correctness-status/correctness-status.component.ts
@@ -2,14 +2,14 @@ import { Component, Input, OnInit } from '@angular/core';
 
 import { CORRECTNESS } from '@a-shared/enums/shared-components';
 import { SVG_TYPE } from '@a-shared/enums/svg';
-import { CorrectnessIndicatorInfo } from '@a-shared/types/correctness-indicator-info';
+import { CorrectnessStatusInfo } from '@a-shared/types/correctness-status-info';
 
 @Component({
   selector: 'quiz-app-correctness-status',
   templateUrl: './correctness-status.component.html'
 })
 export class CorrectnessStatusComponent implements OnInit {
-  @Input() indicatorInfo: CorrectnessIndicatorInfo;
+  @Input() indicatorInfo: CorrectnessStatusInfo;
 
   readonly SVG_TYPE = SVG_TYPE;
 
