ac4c7993df8ca184c21a151ff392374186089c81
Taras <sokha2411@gmail.com>
2023-10-12 18:35:26 +0300

    feat(58): bug fix

--- src/app/shared/components/info-card/info-card.component.ts
+++ src/app/shared/components/info-card/info-card.component.ts
@@ -5,7 +5,7 @@ import { Component, Input } from '@angular/core';
   templateUrl: './info-card.component.html'
 })
 export class InfoCardComponent {
-  @Input() label: string;
-  @Input() subLabel: string;
+  @Input() label = '';
+  @Input() subLabel = '';
   @Input() containerClass: string;
 }
